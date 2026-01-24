a0ffbfe80d54abd1012fdb2f0510d78866dc01a5
vitorpixel-6436 <vitorleitye6436@gmail.com>
2026-01-24 14:10:17 +0000

    Update urls.py

--- music/urls.py
+++ music/urls.py
@@ -5,9 +5,9 @@ app_name = 'music'
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('player/<int:pk>/', views.player, name='player'),
-    path('stream/<int:pk>/', views.stream_music, name='stream'),
-    path('download/<int:pk>/', views.download_music, name='download'),
+    path('player/<uuid:pk>/', views.player, name='player'),
+    path('stream/<uuid:pk>/', views.stream_music, name='stream'),
+    path('download/<uuid:pk>/', views.download_music, name='download'),
     path('upload/', views.upload_page, name='upload_page'),
     path('api/upload/', views.upload_music, name='upload_music'),
 ]
