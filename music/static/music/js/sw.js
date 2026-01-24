20c0ff2f2eedc884a975880b26e62b096e42f710
vitorpixel-6436 <vitorleitye6436@gmail.com>
2026-01-24 23:29:12 +0000

    Create sw.js

---
+++ music/static/music/js/sw.js
@@ -0,0 +1,23 @@
+const CACHE_NAME = 'music-stream-v1';
+const ASSETS_TO_CACHE = [
+    '/',
+    '/static/music/css/modern.css',
+    '/static/music/js/player.js',
+    'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css'
+];
+
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+        caches.open(CACHE_NAME).then((cache) => {
+            return cache.addAll(ASSETS_TO_CACHE);
+        })
+    );
+});
+
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+        caches.match(event.request).then((response) => {
+            return response || fetch(event.request);
+        })
+    );
+});
