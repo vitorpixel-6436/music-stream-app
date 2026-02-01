ebc6e8b83ee015e245a19dd81dd6999de77ea357
vitorpixel-6436 <vitorleitye6436@gmail.com>
2026-02-01 20:11:46 +0000

    feat: export PlayerBar and Playlist components
    - Add PlayerBar and Playlist to __all__
    - Update version to 1.1.0

--- steam_ui/__init__.py
+++ steam_ui/__init__.py
@@ -7,16 +7,18 @@ A modular, reusable UI component library inspired by Steam's design language.
 Features:
 - Glass morphism effects
 - Animated cards and carousels
+- Floating audio player
+- Playlist components
 - Responsive grid layouts
 - Django template integration
 - Standalone usage support
 
-Version: 1.0.0
+Version: 1.1.0
 Author: vitorpixel-6436
 License: MIT
 """
 
-__version__ = '1.0.0'
+__version__ = '1.1.0'
 __author__ = 'vitorpixel-6436'
 
 # Import main components for easy access
@@ -25,6 +27,8 @@ from steam_ui.components import (
     Carousel,
     FeaturedBanner,
     CategoryPills,
+    PlayerBar,
+    Playlist,
 )
 
 __all__ = [
@@ -32,4 +36,6 @@ __all__ = [
     'Carousel', 
     'FeaturedBanner',
     'CategoryPills',
+    'PlayerBar',
+    'Playlist',
 ]
